189dd5507280a0b26d14a25741cff40242106778
denisc1n <denis.l.pedemonte@gmail.com>
2019-03-27 00:10:18 -0300

    Renamed Files

--- ClaseDos/index.php
+++ ClaseDos/index.php
@@ -1,13 +1,12 @@
 <?php
-    require "alumno.php";
-    //$operacionTipo = $_GET["tipo"];
+    require "classes/alumno.php";
+    $operacionTipo = $_GET["tipo"];
     $alumno = new alumno($_POST['nameInput'],$_POST['lastNameInput'],$_POST['idInput'],$_POST['fileIdNumber']);
     $alumnoDos = new alumno($_POST['nameInputDos'],$_POST['lastNameInputDos'],$_POST['idInputDos'],$_POST['fileIdNumberDos']);
     $arrayAlumnos = array($alumno, $alumnoDos);
 
     
-
-    $referenceFile = fopen("myJSONfile.json", 'w');
+    $referenceFile = fopen("data/myJSONfile.json", 'w');
 
     $stringToFile = json_encode($arrayAlumnos);
 
