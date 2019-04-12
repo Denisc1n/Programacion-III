93ccc0692e5435ec1c425743b53e1cff07e231fe
Denisc1n <denis.l.pedemonte@gmail.com>
2019-04-12 15:14:19 -0300

    Post Completo, Get pendiente

--- TPFinal/classes/persona.php
+++ TPFinal/classes/persona.php
@@ -4,11 +4,12 @@ include_once "humano.php";
 class Persona extends Humano
 {
     public $dni;
-
+    public $photoId;
     public function __construct($inputNombre, $inputApellido, $inputDni)
     {
         parent::__construct($inputNombre, $inputApellido);
         $this->dni = $inputDni;
+        $this->photoId = "";
     }
     public function ToJSON(){
         return json_encode($this);
